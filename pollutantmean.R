1bfc081f2d18dc298ae53c102659bc20c853de17
Ellie Kim <keunmee@gmail.com>
2018-01-11 00:08:58 -0800

    done

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,46 +1,44 @@
 
-directory <- "specdata"
-id <- 1:10
-pollutant <- "nitrate"
-pollutantmean(directory, pollutant, id){
-
+#id <- 2:10
+#id
+#min(id)
+pollutantmean <- function(directory, pollutant, id) {
+#id <- 7:9
+#directory <- "specdata"
+#pollutant <- "nitrate"
 a <- sprintf("%s/%03d.csv", directory, id)
-specdatadf <- data.frame(a)
-specdatadf$a <- as.character(specdatadf$a)
 
-for(i in id) { 
-  b <- read.csv(specdatadf$a[i], header = TRUE)
-  if(i == min(id))
-    accb <- b
-  else 
-    accb <- rbind(accb,b )
+print(a[1])
+print(a[length(id)])
+#specdatadf <- data.frame(a)
+#specdatadf$a <- as.character(specdatadf$a)
+
+#i <- min(id)
+#length(id)
+#print(i)
+for(i in 1:length(id)) {
   
-}
-pollutant <- "nitrate"
-
+  b <- read.csv(a[i], header = TRUE)
+ 
+  if(i == 1)
+    {accb <- b
+  print(i)}
+  else 
+    
+    {accb <- rbind(accb,b )
+  print(i)}
+} 
 
 if(pollutant == "nitrate") 
   mean(accb$nitrate, na.rm = TRUE)
 
-else (pollutant == "sulfate")
+else
+#elseif(pollutant == "sulfate") 
   mean(accb$sulfate, na.rm = TRUE)
 
   
-  
-  
-#pollutant4 <- paste0("accb","$",pollutant)
-class(pollutant4)
-
-pollutant4
-accb$nitrate
-pollutant4 == accb$nitrate
-class(accb$nitrate)
-pollutant3 <- as.symbol(pollutant4)
-class(pollutant4)
-pollutant3 <- (pollutant1)
-pollutant3
-str(pollutant3)
-mean(accb$nitrate, na.rm = TRUE)
-mean(pollutant3, na.rm = TRUE)
-?mean
 }
+
+#k <- read.csv("specdata/023.csv", header = TRUE)
+#mean(k$nitrate, na.rm = TRUE)
+#mean(k$sulfate, na.rm = TRUE)
